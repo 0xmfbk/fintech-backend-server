63a060883922b95bb44881acf2b81e6dd32f4324
0xmfbk <103966776+0xmfbk@users.noreply.github.com>
2025-07-18 19:58:13 +0300

    Create offer.py

---
+++ offer.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def get_offers():
+    url = "https://jpcjofsdev.apigw-az-eu.webmethods.io/gateway/Offers/v0.4.3/institution/offers"
+
+    querystring = {"sort":"desc","limit":"10","skip":"0"}
+
+    headers = {
+        "x-idempotency-key": "1",
+        "x-interactions-id": "1",
+        "Authorization": "1",
+        "x-financial-id": "1",
+        "x-jws-signature": "1"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response_text = response.text
+    response_json = json.loads(response_text)
+    return response_json
+
+print(get_offers())
